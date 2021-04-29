53ac0660d2134c5d13bbefaca1c85b3a9ce8f050
patt26 <kharchepratik123@gmail.com>
2021-04-29 22:43:28 +0530

    Added : Regex Final User Case

---
+++ RegexFinal/Pattern.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexFinal
+{
+    class Name
+    {
+        string Name_Validator = "^[A-Z]{1}[a-z]{3,}$";
+
+        public bool ValidateName(string result)
+        {
+            return Regex.IsMatch(result, Name_Validator);
+        }
+        public bool ValidateName1(string result1)
+        {
+            return Regex.IsMatch(result1, Name_Validator);
+        }
+    }
+    public class Password
+    {
+        string PasswordValidator = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+]).{8,}$";
+
+        public bool Validate(string name)
+        {
+            return Regex.IsMatch(name, PasswordValidator);
+        }
+    }
+    class PhoneNum
+    {
+        string PhoneNoValidator = "^[91]{2}[ ]*[0-9]{10}$";
+
+        public bool PhoneValidator(string name)
+        {
+            return Regex.IsMatch(name, PhoneNoValidator);
+        }
+    }
+
+    class EmailId
+    {
+        string EmailIdValidator = "^[a-zA-Z0-9]+([.]{1}[a-zA-Z0-9]+)?[@]{1}[a-zA-Z]+[.][A-Za-z]{3}$";
+
+        public bool ValidateEmail(string name)
+        {
+            return Regex.IsMatch(name, EmailIdValidator);
+        }
+    }
+}
+
---
+++ RegexFinal/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexFinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Choice: \n 1: Name Validator\n 2: Email Validator\n 3: Mobile Number Validator\n 4: Password Validator");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Name pattern = new Name();
+                    bool result = pattern.ValidateName(FirstName());
+                    Console.WriteLine(result);
+                    bool result1 = pattern.ValidateName(LastName());
+                    Console.Write(result1);
+                    break;
+                case 2:
+                    EmailId pattern1 = new EmailId();
+                    Console.Write("Enter Email ID - ");
+                    string name = Console.ReadLine();
+                    bool result2 = pattern1.ValidateEmail(name);
+                    Console.WriteLine(result2);
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    PhoneNum pattern3 = new PhoneNum();
+                    Console.Write("Enter Mobile Number - ");
+                    string name1 = Console.ReadLine();
+                    bool result3 = pattern3.PhoneValidator(name1);
+                    Console.WriteLine(result3);
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    Password pattern4 = new Password();
+                    Console.Write("Enter Password - ");
+                    string name2 = Console.ReadLine();
+                    bool result4 = pattern4.Validate(name2);
+                    Console.WriteLine(result4);
+                    Console.ReadKey();
+                    break;
+            }
+        }
+        public static string LastName()
+         {
+            Console.WriteLine("Enter Last Name= ");
+            string LName = Console.ReadLine();
+            return LName;
+         }
+
+        public static string FirstName()
+        {
+            Console.WriteLine("Enter a First Name = ");
+            string FName = Console.ReadLine();
+            return FName;
+        }
+    }
+}
+    
